a49c15504ee91432745c748dc9471e3fe33a990b
Philipp Obrivanov <milliardex@gmail.com>
2022-02-11 10:17:58 +0300

    v. 1.0.0

--- magic_ball.rb
+++ magic_ball.rb
@@ -1,33 +1,12 @@
-greetings =
-  [
-    'Hello, dear friend. I answer your question ...',
-    'Whoever asks will receive the answer:',
-    'Hello mortal. Today the answer for you is:'
-  ]
+require 'yaml'
 
-answers =
-  [
-    'It is certain.',
-    'It is decidedly so.',
-    'Without a doubt.',
-    'Yes definitely.',
-    'You may rely on it.',
-    'As I see it, yes.',
-    'Most likely.',
-    'Outlook good.',
-    'Yes.',
-    'Signs point to yes.',
-    'Reply hazy, try again.',
-    'Ask again later.',
-    'Better not tell you now.',
-    'Cannot predict now.',
-    'Concentrate and ask again.',
-    'Don\'t count on it.',
-    'My reply is no.',
-    'My sources say no.',
-    'Outlook not so good.',
-    'Very doubtful.'
-  ]
+puts 'Choose your language: (1 - en, 2 - рус):' 
+
+# English default if input is incorrect
+locale = gets.to_i == 2 ? 'ru' : 'en'
+
+greetings = YAML.load_file("locales/#{locale}.greetings.yml")
+answers = YAML.load_file("locales/#{locale}.answers.yml")
 
 puts greetings.sample
 puts
